18530967caa7331a2d85700704225126c5a6ae05
Alef Daniel <83924026+Alef-Daniel@users.noreply.github.com>
2024-12-17 18:42:05 +0000

    feat: adiciona struct Buyer

--- internal/model/buyer.go
+++ internal/model/buyer.go
@@ -1,4 +1,8 @@
 package model
 
 type Buyer struct {
+	Id int `json: "id"`
+	CardNumberId int `json: "card_number_id"`
+	FirstName string `json: "first_name"`
+	LastName string `json: "last_name"`
 }
