978b35c48a0f855ebd41d35868478623161f65f9
Alef Daniel <83924026+Alef-Daniel@users.noreply.github.com>
2024-12-18 13:55:28 +0000

    fix(handler): corrigir tags JSON e estrutura de resposta na API de buyers

--- internal/handler/buyer.go
+++ internal/handler/buyer.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/bootcamp-go/web/response"
@@ -12,26 +12,54 @@ type BuyerHandler struct {
 	svc *service.BuyerService
 }
 
+type ResponseBuyerJson struct {
+	Id           int    `json:"id"`
+	CardNumberId int    `json:"card_number_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+}
+
+type Data struct {
+	Data []ResponseBuyerJson `json:"data"`
+}
+
 func NewBuyerHandler(svc *service.BuyerService) *BuyerHandler {
 	return &BuyerHandler{svc}
 }
 
 func (bh *BuyerHandler) HandlerGetAllBuyers(w http.ResponseWriter, r *http.Request) {
-	
-	fmt.Println("Bateu aqui")
+
 	buyers, err := bh.svc.GetAllBuyer()
-
-	
-
 	if err != nil {
-		response.JSON(w, http.StatusBadRequest, map[string]any{
+
+		response.JSON(w, http.StatusNotFound, map[string]any{
 			"message": "Não há buyers cadastrados",
 		})
 		return
 	}
 
-	response.JSON(w, http.StatusOK, map[string]any{
-		"message": "Success",
-		"Data":    buyers,
-	})
+	var dataBuyers []ResponseBuyerJson
+	for _, b := range buyers {
+
+		dataBuyers = append(dataBuyers, ResponseBuyerJson{
+			Id:           b.Id,
+			CardNumberId: b.CardNumberId,
+			FirstName:    b.FirstName,
+			LastName:     b.LastName,
+		})
+	}
+
+	data := Data{
+		Data: dataBuyers,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err = json.NewEncoder(w).Encode(data)
+	if err != nil {
+
+		http.Error(w, "Erro ao serializar os dados", http.StatusInternalServerError)
+		return
+	}
 }
--- internal/model/buyer.go
+++ internal/model/buyer.go
@@ -1,8 +1,8 @@
 package model
 
 type Buyer struct {
-	Id           int    `json: "id"`
-	CardNumberId int    `json: "card_number_id"`
-	FirstName    string `json: "first_name"`
-	LastName     string `json: "last_name"`
+	Id           int
+	CardNumberId int
+	FirstName    string
+	LastName     string
 }
