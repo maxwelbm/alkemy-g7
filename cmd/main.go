2ec5c34b81b85287f5292fbad9a72cbd31f76542
thavieira_meli <thiago.avieira@mercadolivre.com>
2024-12-18 10:45:13 -0300

    feat: inserting in main route Handler GetAllWareHouse

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,37 @@
-package cmd
+package main
 
 import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/maxwelbm/alkemy-g7.git/cmd/database"
+	"github.com/maxwelbm/alkemy-g7.git/internal/handler"
+	"github.com/maxwelbm/alkemy-g7.git/internal/repository"
+	"github.com/maxwelbm/alkemy-g7.git/internal/service"
 )
 
 func main() {
+	dbWareHouse := database.CreateDatabase()
+	rpWareHouse := repository.NewWareHouseMap(*dbWareHouse)
+	srvWareHouse := service.NewWareHouDefault(*rpWareHouse)
+	hdWareHouse := handler.NewWareHouseHandlerDefault(*srvWareHouse)
 
-	rt := initRoutes()
+	rt := initRoutes(hdWareHouse)
 
 	if err := http.ListenAndServe(":8080", rt); err != nil {
 		panic(err)
 	}
 }
 
-func initRoutes() *chi.Mux {
+func initRoutes(hd *handler.WarehouseHandler) *chi.Mux {
 	rt := chi.NewRouter()
 
 	rt.Route("/api/v1/warehouses", func(r chi.Router) {
-		rt.Get("/", nil)
-		rt.Get("/{id}", nil)
-		rt.Post("/", nil)
-		rt.Patch("/{id}", nil)
-		rt.Delete("/{id}", nil)
+		r.Get("/", hd.GetAllWareHouse())
+		r.Get("/{id}", nil)
+		r.Post("/", nil)
+		r.Patch("/{id}", nil)
+		r.Delete("/{id}", nil)
 	})
 
 	rt.Route("/api/v1/sections", func(r chi.Router) {
@@ -35,14 +43,6 @@ func initRoutes() *chi.Mux {
 	})
 
 	rt.Route("/api/v1/products", func(r chi.Router) {
-		rt.Get("/", nil)
-		rt.Get("/{id}", nil)
-		rt.Post("/", nil)
-		rt.Patch("/{id}", nil)
-		rt.Delete("/{id}", nil)
-	})
-
-	rt.Route("/api/v1/buyers", func(r chi.Router) {
 		rt.Get("/", nil)
 		rt.Get("/{id}", nil)
 		rt.Post("/", nil)
