9f0d86b2152387b5d139c6d441bb0bc4df113dbd
danielagff <daniel.agff@gmail.com>
2024-12-17 21:41:18 -0300

    Implement repository and service for products; add findAll and findById methods

---
+++ internal/handler/product.go
@@ -0,0 +1,42 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/bootcamp-go/web/response"
+	"github.com/maxwelbm/alkemy-g7.git/internal/service/interfaces"
+)
+
+
+
+type ProductHandler struct {
+	ProductService interfaces.IProductService
+}
+
+func NewProductHandler(ps interfaces.IProductService) *ProductHandler {
+	return &ProductHandler{ProductService: ps}
+}
+
+func (ph *ProductHandler) GetAllProducts(w http.ResponseWriter, r *http.Request) {
+    products, err := ph.ProductService.GetAllProducts()
+    var responseBody map[string]interface{}
+
+	fmt.Println(products)
+
+
+    if err != nil {
+        responseBody = map[string]interface{}{
+            "error": err.Error(),
+        }
+		fmt.Print(err.Error())
+        response.JSON(w, http.StatusNotFound, responseBody)
+        return
+    }
+
+    responseBody = map[string]interface{}{
+        "data": products,
+    }
+
+    response.JSON(w, http.StatusOK, responseBody)
+}
