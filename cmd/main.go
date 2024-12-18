889b8fa628e20680018f45d602698929756db47e
julidsilva_meli <juliana.udsilva@mercadolivre.com>
2024-12-17 23:45:04 -0300

    Create handler and adjust router

--- cmd/main.go
+++ cmd/main.go
@@ -1,69 +1,76 @@
-package cmd
+package main
 
 import (
 	"net/http"
-
 	"github.com/go-chi/chi/v5"
+	"github.com/maxwelbm/alkemy-g7.git/cmd/database"
+	"github.com/maxwelbm/alkemy-g7.git/internal/handler"
+	"github.com/maxwelbm/alkemy-g7.git/internal/repository"
+	"github.com/maxwelbm/alkemy-g7.git/internal/service"
 )
 
 func main() {
 
-	rt := initRoutes()
+	db := database.CreateDatabase()
+	repository := repository.CreateRepositorySellers(db.TbSellers)
+	service := service.CreateServiceSellers(*repository)
+	handler := handler.CreateHandlerSellers(*service)
 
+	rt := initRoutes(*handler)
 	if err := http.ListenAndServe(":8080", rt); err != nil {
 		panic(err)
 	}
 }
 
-func initRoutes() *chi.Mux {
+func initRoutes(handler handler.SellersController) *chi.Mux {
 	rt := chi.NewRouter()
 
 	rt.Route("/api/v1/warehouses", func(r chi.Router) {
-		rt.Get("/", nil)
-		rt.Get("/{id}", nil)
-		rt.Post("/", nil)
-		rt.Patch("/{id}", nil)
-		rt.Delete("/{id}", nil)
+		r.Get("/", nil)
+		r.Get("/{id}", nil)
+		r.Post("/", nil)
+		r.Patch("/{id}", nil)
+		r.Delete("/{id}", nil)
 	})
 
 	rt.Route("/api/v1/sections", func(r chi.Router) {
-		rt.Get("/", nil)
-		rt.Get("/{id}", nil)
-		rt.Post("/", nil)
-		rt.Patch("/{id}", nil)
-		rt.Delete("/{id}", nil)
+		r.Get("/", nil)
+		r.Get("/{id}", nil)
+		r.Post("/", nil)
+		r.Patch("/{id}", nil)
+		r.Delete("/{id}", nil)
 	})
 
 	rt.Route("/api/v1/products", func(r chi.Router) {
-		rt.Get("/", nil)
-		rt.Get("/{id}", nil)
-		rt.Post("/", nil)
-		rt.Patch("/{id}", nil)
-		rt.Delete("/{id}", nil)
+		r.Get("/", nil)
+		r.Get("/{id}", nil)
+		r.Post("/", nil)
+		r.Patch("/{id}", nil)
+		r.Delete("/{id}", nil)
 	})
 
 	rt.Route("/api/v1/buyers", func(r chi.Router) {
-		rt.Get("/", nil)
-		rt.Get("/{id}", nil)
-		rt.Post("/", nil)
-		rt.Patch("/{id}", nil)
-		rt.Delete("/{id}", nil)
+		r.Get("/", nil)
+		r.Get("/{id}", nil)
+		r.Post("/", nil)
+		r.Patch("/{id}", nil)
+		r.Delete("/{id}", nil)
 	})
 
-	rt.Route("/api/v1/buyers", func(r chi.Router) {
-		rt.Get("/", nil)
-		rt.Get("/{id}", nil)
-		rt.Post("/", nil)
-		rt.Patch("/{id}", nil)
-		rt.Delete("/{id}", nil)
+	rt.Route("/api/v1/employee", func(r chi.Router) {
+		r.Get("/", nil)
+		r.Get("/{id}", nil)
+		r.Post("/", nil)
+		r.Patch("/{id}", nil)
+		r.Delete("/{id}", nil)
 	})
 
 	rt.Route("/api/v1/sellers", func(r chi.Router) {
-		rt.Get("/", nil)
-		rt.Get("/{id}", nil)
-		rt.Post("/", nil)
-		rt.Patch("/{id}", nil)
-		rt.Delete("/{id}", nil)
+		r.Get("/", handler.GetAllSellers())
+		r.Get("/{id}", nil)
+		r.Post("/", nil)
+		r.Patch("/{id}", nil)
+		r.Delete("/{id}", nil)
 	})
 
 	return rt
