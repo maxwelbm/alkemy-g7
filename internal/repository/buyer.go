63f25784a8c0c959d20aac9b7ebb7b0cb5fd339a
Alef Daniel <83924026+Alef-Daniel@users.noreply.github.com>
2024-12-18 00:10:54 +0000

    fix: validation size tbBuyer

--- internal/repository/buyer.go
+++ internal/repository/buyer.go
@@ -1,21 +1,25 @@
 package repository
 
 import (
+	"fmt"
+
+	"github.com/maxwelbm/alkemy-g7.git/cmd/database"
 	"github.com/maxwelbm/alkemy-g7.git/internal/model"
-	"github.com/maxwelbm/alkemy-g7.git/pkg/custom_error"
 )
 
-
 type BuyerRepository struct {
-	dbBuyer map[int]model.Buyer
+	dbBuyer database.Database
 }
 
-
-func NewBuyerRepository(db map[int]model.Buyer) *BuyerRepository{
+func NewBuyerRepository(db database.Database) *BuyerRepository {
 	return &BuyerRepository{dbBuyer: db}
 }
 
+func (br *BuyerRepository) Get() (map[int]model.Buyer, error) {
 
-func (br *BuyerRepository) Get() (map[int]model.Buyer, error){
-	return br.dbBuyer, nil
+	if len(br.dbBuyer.TbBuyer) == 0 {
+		return nil, fmt.Errorf("no buyers found")
+	}
+	return br.dbBuyer.TbBuyer, nil
+
 }
