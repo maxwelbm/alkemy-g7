264fcb7f837f437f6a500352454fb448d76892ba
danielagff <daniel.agff@gmail.com>
2024-12-17 21:34:53 -0300

    Implement product repository and service interfaces

--- internal/repository/interfaces/iproducts.go
+++ internal/repository/interfaces/iproducts.go
@@ -1,9 +1,11 @@
 package interfaces
 
+import "github.com/maxwelbm/alkemy-g7.git/internal/model"
+
 type IProductsRepo interface {
-	Get() (map[int]model.Products, error)
-	GetById(id int) (model.Products, error)
-	Post(product model.Products) (model.Products, error)
-	Update(id int, product model.Products) (model.Products, error)
-	Delete(id int) error
+	GetAll() (map[int]model.Product, error)
+	GetById(id int) (model.Product, error)
+	Create(product model.Product) (model.Product, error)
+	Update(id int, product model.Product) (model.Product, error)
+	Delete(id int) error 
 }
