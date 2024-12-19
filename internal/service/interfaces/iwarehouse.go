d3e092c3a87dc2bf5cfac69ed0f9a042d690450f
thavieira_meli <thiago.avieira@mercadolivre.com>
2024-12-19 11:49:18 -0300

    refactor: refactoring name method Post for PostWareHouse in interfaceService

--- internal/service/interfaces/iwarehouse.go
+++ internal/service/interfaces/iwarehouse.go
@@ -5,7 +5,7 @@ import "github.com/maxwelbm/alkemy-g7.git/internal/model"
 type IWarehouseService interface {
 	GetAllWareHouse() (w map[int]model.WareHouse, err error)
 	GetByIdWareHouse(id int) (w model.WareHouse, err error)
-	Post(warehouse model.WareHouse) (w model.WareHouse, err error)
+	PostWareHouse(warehouse model.WareHouse) (w model.WareHouse, err error)
 	Update(id int, warehouse model.WareHouse) (w model.WareHouse, err error)
 	DeleteByIdWareHouse(id int) error
 }
