9fbb272269efe643f094583213bef6bab6005cf7
thavieira_meli <thiago.avieira@mercadolivre.com>
2024-12-18 23:19:07 -0300

    feat: create news interfaces in wareHouse model for res and json

--- internal/model/warehouse.go
+++ internal/model/warehouse.go
@@ -8,3 +8,16 @@ type WareHouse struct {
 	MinimunCapacity    int
 	MinimunTemperature int
 }
+
+type WareHouseJson struct {
+	Id                 int    `json:"id"`
+	Address            string `json:"address"`
+	Telephone          string `json:"telephone"`
+	WareHouseCode      string `json:"warehouse_code"`
+	MinimunCapacity    int    `json:"minimun_capacity"`
+	MinimunTemperature int    `json:"minimun_temperature"`
+}
+
+type WareHouseRes struct {
+	Data []WareHouseJson `json:"data"`
+}
