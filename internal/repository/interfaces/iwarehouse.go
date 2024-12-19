a632ec3183ed1df64f5528c2c4b56006a9c2f1ac
thavieira_meli <thiago.avieira@mercadolivre.com>
2024-12-19 15:13:27 -0300

    feat: implement updateWarehouse in interface repository

--- internal/repository/interfaces/iwarehouse.go
+++ internal/repository/interfaces/iwarehouse.go
@@ -6,6 +6,6 @@ type IWarehouseRepo interface {
 	GetAllWareHouse() (w map[int]model.WareHouse, err error)
 	GetByIdWareHouse(id int) (w model.WareHouse, err error)
 	PostWareHouse(warehouse model.WareHouse) (w model.WareHouse, err error)
-	Update(id int, warehouse model.WareHouse) (w model.WareHouse, err error)
+	UpdateWareHouse(id int, warehouse model.WareHouse) (w model.WareHouse, err error)
 	DeleteByIdWareHouse(id int) error
 }
