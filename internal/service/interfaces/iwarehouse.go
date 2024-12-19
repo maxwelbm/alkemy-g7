467ad9dbed8cb357742b49a13250be9400006759
thavieira_meli <thiago.avieira@mercadolivre.com>
2024-12-18 23:19:48 -0300

    refactor: refactoring format for return in interfaces warehouseService

--- internal/service/interfaces/iwarehouse.go
+++ internal/service/interfaces/iwarehouse.go
@@ -3,8 +3,8 @@ package interfaces
 import "github.com/maxwelbm/alkemy-g7.git/internal/model"
 
 type IWarehouseService interface {
-	Get() (map[int]model.WareHouse, error)
-	GetById(id int) (model.WareHouse, error)
+	Get() (map[int]model.WareHouseJson, error)
+	GetById(id int) (model.WareHouseJson, error)
 	Post(warehouse model.WareHouse) (model.WareHouse, error)
 	Update(id int, warehouse model.WareHouse) (model.WareHouse, error)
 	Delete(id int) error
