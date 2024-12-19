6511c34009e02f5f675848c5f852529aec5ecfff
thavieira_meli <thiago.avieira@mercadolivre.com>
2024-12-18 23:21:00 -0300

    feat: create new handler getbyId warehouse

--- internal/handler/warehouse.go
+++ internal/handler/warehouse.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/bootcamp-go/web/response"
+	"github.com/go-chi/chi/v5"
+	"github.com/maxwelbm/alkemy-g7.git/internal/model"
 	"github.com/maxwelbm/alkemy-g7.git/internal/service"
 )
 
@@ -24,9 +27,45 @@ func (h *WarehouseHandler) GetAllWareHouse() http.HandlerFunc {
 			return
 		}
 
-		response.JSON(w, http.StatusOK, map[string]any{
-			"message": "success",
-			"data":    wareHouse,
-		})
+		var data []model.WareHouseJson
+
+		for _, value := range wareHouse {
+			data = append(data, model.WareHouseJson{
+				Id:                 value.Id,
+				Address:            value.Address,
+				Telephone:          value.Telephone,
+				WareHouseCode:      value.WareHouseCode,
+				MinimunCapacity:    value.MinimunCapacity,
+				MinimunTemperature: value.MinimunTemperature,
+			})
+		}
+		responseJson := model.WareHouseRes{Data: data}
+
+		response.JSON(w, http.StatusOK, responseJson)
+
 	}
+}
+
+func (h *WarehouseHandler) GetWareHouseById() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+		if err != nil {
+			response.JSON(w, http.StatusBadRequest, "invalid id")
+			return
+		}
+
+		warehouse, err := h.srv.GetByIdWareHouse(id)
+
+		if err != nil {
+			response.JSON(w, http.StatusNotFound, "warehouse not found")
+			return
+		}
+
+		response.JSON(w, http.StatusOK, map[string]any{
+			"data": warehouse,
+		})
+
+	}
+
 }
