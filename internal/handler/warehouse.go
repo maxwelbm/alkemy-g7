2264da119a69703ebf875813d2100ea2286bd32e
Alef Daniel <83924026+Alef-Daniel@users.noreply.github.com>
2024-12-20 17:08:39 +0000

    Fix: Change Name ErrorResponseJson in Warehouse

--- internal/handler/warehouse.go
+++ internal/handler/warehouse.go
@@ -18,7 +18,7 @@ type ResponseWareHouseJson struct {
 	Data    any    `json:"data"`
 }
 
-type ErrorResponse struct {
+type ErrorResponseJson struct {
 	Message string `json:"Message"`
 }
 
@@ -79,7 +79,7 @@ func (h *WarehouseHandler) GetWareHouseById() http.HandlerFunc {
 		warehouse, err := h.srv.GetByIdWareHouse(id)
 
 		if err != nil {
-			response.JSON(w, http.StatusNotFound, ErrorResponse{
+			response.JSON(w, http.StatusNotFound, ErrorResponseJson{
 				Message: "warehouse not found",
 			})
 			return
@@ -147,7 +147,7 @@ func (h *WarehouseHandler) PostWareHouse() http.HandlerFunc {
 		})
 
 		if err != nil {
-			response.JSON(w, http.StatusBadRequest, ErrorResponse{
+			response.JSON(w, http.StatusBadRequest, ErrorResponseJson{
 				Message: err.Error(),
 			})
 			return
@@ -191,7 +191,7 @@ func (h *WarehouseHandler) UpdateWareHouse() http.HandlerFunc {
 		})
 
 		if err != nil {
-			response.JSON(w, http.StatusBadRequest, ErrorResponse{
+			response.JSON(w, http.StatusBadRequest, ErrorResponseJson{
 				Message: err.Error(),
 			})
 			return
