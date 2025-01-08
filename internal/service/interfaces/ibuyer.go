22893ea6f5aed723cb807ca295fe4621ff522658
Alef <alefsa_daniel@hotmail.com>
2025-01-08 18:39:54 -0300

    fix: change signature and logic in deleteBuyerByID service

--- internal/service/interfaces/ibuyer.go
+++ internal/service/interfaces/ibuyer.go
@@ -5,7 +5,7 @@ import "github.com/maxwelbm/alkemy-g7.git/internal/model"
 type IBuyerservice interface {
 	GetAllBuyer() (buyers []model.Buyer, err error)
 	GetBuyerByID(id int) (buyer model.Buyer, err error)
-	DeleteBuyerByID(id int) error
+	DeleteBuyerByID(id int) (err error)
 	CreateBuyer(newBuyer model.Buyer) (model.Buyer, error)
 	UpdateBuyer(id int, newBuyer model.Buyer) (model.Buyer, error)
 }
