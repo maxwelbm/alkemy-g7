f97e23fd8f3e95db5bb1fe2396a1197f855e45c9
Alef <alefsa_daniel@hotmail.com>
2025-01-08 17:44:13 -0300

    fix: implement service and change Signature for service Buyer

--- internal/service/interfaces/ibuyer.go
+++ internal/service/interfaces/ibuyer.go
@@ -3,7 +3,7 @@ package interfaces
 import "github.com/maxwelbm/alkemy-g7.git/internal/model"
 
 type IBuyerservice interface {
-	GetAllBuyer() (map[int]model.Buyer, error)
+	GetAllBuyer() (buyers []model.Buyer, err error)
 	GetBuyerByID(id int) (model.Buyer, error)
 	DeleteBuyerByID(id int) error
 	CreateBuyer(newBuyer model.Buyer) (model.Buyer, error)
