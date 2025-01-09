c94158c5b35dd3ee98c95d973d6f6027aae002da
Alef <alefsa_daniel@hotmail.com>
2025-01-09 10:53:32 -0300

    fix: change responseBody e verify errors in handler create Buyer and add method validateEmptyFields in model buyer

--- internal/model/buyer.go
+++ internal/model/buyer.go
@@ -1,8 +1,36 @@
 package model
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Buyer struct {
-	Id           int    `json: "id"`
-	CardNumberId string    `json: "card_number_id"`
-	FirstName    string `json: "first_name"`
-	LastName     string `json: "last_name"`
+	Id           int    `json:"id"`
+	CardNumberId string `json:"card_number_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+}
+
+func (b *Buyer) ValidateEmptyFields() error {
+	var fieldsEmpty []string
+
+	if b.CardNumberId == "" {
+		fieldsEmpty = append(fieldsEmpty, "card_number_id")
+	}
+	if b.FirstName == "" {
+		fieldsEmpty = append(fieldsEmpty, "first_name")
+	}
+	if b.LastName == "" {
+		fieldsEmpty = append(fieldsEmpty, "last_name")
+	}
+
+	if len(fieldsEmpty) == 0 {
+		return nil
+	} else if len(fieldsEmpty) == 1 {
+		return fmt.Errorf("Field %s cannot be empty", strings.Join(fieldsEmpty, ","))
+	}
+
+	return fmt.Errorf("Fields %s cannot be empty", strings.Join(fieldsEmpty, ","))
+
 }
