ebc687482034512fd4d5a5fe209f9e8317625b32
Alexandre Junior <atlopesjunior@gmail.com>
2025-01-10 07:28:01 -0300

    refactor: changing database to mySQL and fixing section model types

--- internal/handler/section.go
+++ internal/handler/section.go
@@ -19,15 +19,15 @@ type SectionsJSON struct {
 }
 
 type SectionJSON struct {
-	ID                 int `json:"id"`
-	SectionNumber      int `json:"section_number"`
-	CurrentTemperature int `json:"current_temperature"`
-	MinimumTemperature int `json:"minimum_temperature"`
-	CurrentCapacity    int `json:"current_capacity"`
-	MinimumCapacity    int `json:"minimum_capacity"`
-	MaximumCapacity    int `json:"maximum_capacity"`
-	WarehouseID        int `json:"warehouse_id"`
-	ProductTypeID      int `json:"product_type_id"`
+	ID                 int     `json:"id"`
+	SectionNumber      string  `json:"section_number"`
+	CurrentTemperature float64 `json:"current_temperature"`
+	MinimumTemperature float64 `json:"minimum_temperature"`
+	CurrentCapacity    int     `json:"current_capacity"`
+	MinimumCapacity    int     `json:"minimum_capacity"`
+	MaximumCapacity    int     `json:"maximum_capacity"`
+	WarehouseID        int     `json:"warehouse_id"`
+	ProductTypeID      int     `json:"product_type_id"`
 }
 
 func CreateHandlerSections(sv *service.SectionService) *SectionController {
--- internal/repository/sections.go
+++ internal/repository/sections.go
@@ -1,10 +1,10 @@
 package repository
 
 import (
+	"database/sql"
 	"errors"
 
 	"github.com/maxwelbm/alkemy-g7.git/internal/model"
-	"github.com/maxwelbm/alkemy-g7.git/pkg/database"
 )
 
 var (
@@ -13,81 +13,100 @@ var (
 )
 
 type SectionRepository struct {
-	dbSection database.Database
+	db *sql.DB
 }
 
-func CreateRepositorySections(db database.Database) *SectionRepository {
-	return &SectionRepository{dbSection: db}
+func CreateRepositorySections(db *sql.DB) *SectionRepository {
+	return &SectionRepository{db}
 }
 
 func (r *SectionRepository) Get() (sections []model.Section, err error) {
-	sections = []model.Section{}
+	queryGetAll := "SELECT `id`, `section_number`, `current_temperature`, `minimum_temperature`, `current_capacity`, `minimum_capacity`, `maximum_capacity`, `warehouse_id`, `product_type_id` FROM `sections`"
+	rows, err := r.db.Query(queryGetAll)
+	if err != nil {
+		return
+	}
 
-	for _, section := range r.dbSection.TbSections {
+	for rows.Next() {
+		var section model.Section
+		err = rows.Scan(&section.ID, &section.SectionNumber, &section.CurrentTemperature, &section.MinimumTemperature, &section.CurrentCapacity, &section.MinimumCapacity, &section.MaximumCapacity, &section.WarehouseID, &section.ProductTypeID)
+		if err != nil {
+			return
+		}
 		sections = append(sections, section)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return
 	}
 
 	return
 }
 
 func (r *SectionRepository) GetById(id int) (section model.Section, err error) {
-	if _, exists := r.dbSection.TbSections[id]; !exists {
-		err = NotFoundError
-		return
-	}
-	section = r.dbSection.TbSections[id]
-	return
+	// if _, exists := r.dbSection.TbSections[id]; !exists {
+	// 	err = NotFoundError
+	// 	return
+	// }
+	// section = r.dbSection.TbSections[id]
+	// return
+	return model.Section{}, nil
 }
 
 func (r *SectionRepository) Post(section model.Section) (s model.Section, err error) {
-	lastId := getLastId(r.dbSection.TbSections)
-	sectionExists := sectionNumberExists(section.SectionNumber, r)
-	if _, exists := r.dbSection.TbSections[section.ID]; exists {
-		err = ConflictError
-		return
-	}
-	if sectionExists {
-		err = ConflictError
-		return
-	}
-	section.ID = lastId
-	r.dbSection.TbSections[section.ID] = section
-	s = section
-	return
+	// lastId := getLastId(r.dbSection.TbSections)
+	// sectionExists := sectionNumberExists(section.SectionNumber, r)
+	// if _, exists := r.dbSection.TbSections[section.ID]; exists {
+	// 	err = ConflictError
+	// 	return
+	// }
+	// if sectionExists {
+	// 	err = ConflictError
+	// 	return
+	// }
+	// section.ID = lastId
+	// r.dbSection.TbSections[section.ID] = section
+	// s = section
+	// return
+	return model.Section{}, nil
 }
 
 func (r *SectionRepository) Update(id int, section model.Section) (newSec model.Section, err error) {
-	if _, exists := r.dbSection.TbSections[id]; !exists {
-		err = NotFoundError
-		return
-	}
-	sectionExists := sectionNumberExists(section.SectionNumber, r)
-	if r.dbSection.TbSections[id].SectionNumber != section.SectionNumber {
-		if sectionExists {
-			err = ConflictError
-			return
-		}
-	}
-	newSec = section
-	r.dbSection.TbSections[id] = newSec
-	return
+	// if _, exists := r.dbSection.TbSections[id]; !exists {
+	// 	err = NotFoundError
+	// 	return
+	// }
+	// sectionExists := sectionNumberExists(section.SectionNumber, r)
+	// if r.dbSection.TbSections[id].SectionNumber != section.SectionNumber {
+	// 	if sectionExists {
+	// 		err = ConflictError
+	// 		return
+	// 	}
+	// }
+	// newSec = section
+	// r.dbSection.TbSections[id] = newSec
+	// return
+	return model.Section{}, nil
 }
 
 func (r *SectionRepository) Delete(id int) (err error) {
-	if _, exists := r.dbSection.TbSections[id]; !exists {
-		err = NotFoundError
-		return
-	}
-	delete(r.dbSection.TbSections, id)
-	return
+	// if _, exists := r.dbSection.TbSections[id]; !exists {
+	// 	err = NotFoundError
+	// 	return
+	// }
+	// delete(r.dbSection.TbSections, id)
+	// return
+	return err
 }
 
-func sectionNumberExists(sectionNumber int, sr *SectionRepository) bool {
-	for _, section := range sr.dbSection.TbSections {
-		if section.SectionNumber == sectionNumber {
-			return true
-		}
-	}
+func sectionNumberExists(sectionNumber string, sr *SectionRepository) bool {
+	// for _, section := range sr.dbSection.TbSections {
+	// 	if section.SectionNumber == sectionNumber {
+	// 		return true
+	// 	}
+	// }
+	// return false
 	return false
 }
 
