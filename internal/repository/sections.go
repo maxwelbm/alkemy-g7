6cf7b468d4d8bbedbae207134b906abb920f2d2f
Alexandre Junior <atlopesjunior@gmail.com>
2025-01-13 10:51:25 -0300

    refactor: changing post function to use sql

--- internal/repository/sections.go
+++ internal/repository/sections.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/maxwelbm/alkemy-g7.git/internal/model"
 	"github.com/maxwelbm/alkemy-g7.git/pkg/custom_error"
 )
@@ -53,22 +55,33 @@ func (r *SectionRepository) GetById(id int) (section model.Section, err error) {
 	return
 }
 
-func (r *SectionRepository) Post(section model.Section) (s model.Section, err error) {
-	// lastId := getLastId(r.dbSection.TbSections)
-	// sectionExists := sectionNumberExists(section.SectionNumber, r)
-	// if _, exists := r.dbSection.TbSections[section.ID]; exists {
-	// 	err = ConflictError
-	// 	return
-	// }
-	// if sectionExists {
-	// 	err = ConflictError
-	// 	return
-	// }
-	// section.ID = lastId
-	// r.dbSection.TbSections[section.ID] = section
-	// s = section
-	// return
-	return model.Section{}, nil
+func (r *SectionRepository) Post(section *model.Section) (s model.Section, err error) {
+	queryPost := "INSERT INTO `sections` (`section_number`, `current_temperature`, `minimum_temperature`, `current_capacity`, `minimum_capacity`, `maximum_capacity`, `warehouse_id`, `product_type_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+
+	result, err := r.db.Exec(queryPost, (*section).SectionNumber, (*section).CurrentTemperature, (*section).MinimumTemperature, (*section).CurrentCapacity, (*section).MinimumCapacity, (*section).MaximumCapacity, (*section).WarehouseID, (*section).ProductTypeID)
+	if err != nil {
+		var mysqlErr *mysql.MySQLError
+		if errors.As(err, &mysqlErr) {
+			switch mysqlErr.Number {
+			case 1062:
+				err = custom_error.ConflictErrorSection
+			default:
+			}
+			return
+		}
+		return
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return
+	}
+
+	(*section).ID = int(id)
+
+	s, _ = r.GetById(int(id))
+
+	return
 }
 
 func (r *SectionRepository) Update(id int, section model.Section) (newSec model.Section, err error) {
@@ -97,24 +110,4 @@ func (r *SectionRepository) Delete(id int) (err error) {
 	// delete(r.dbSection.TbSections, id)
 	// return
 	return err
-}
-
-func sectionNumberExists(sectionNumber string, sr *SectionRepository) bool {
-	// for _, section := range sr.dbSection.TbSections {
-	// 	if section.SectionNumber == sectionNumber {
-	// 		return true
-	// 	}
-	// }
-	// return false
-	return false
-}
-
-func getLastId(db map[int]model.Section) int {
-	lastId := 0
-	for _, section := range db {
-		if section.ID > lastId {
-			lastId = section.ID
-		}
-	}
-	return lastId + 1
 }
