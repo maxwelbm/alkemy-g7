a34915cc91ee72b119a96dc2fdef51b9699c9cc2
Thiago Vieira <thiivieira@gmail.com>
2025-01-14 11:20:22 -0300

    feat: create new interface repository carries

---
+++ internal/repository/interfaces/icarries.go
@@ -0,0 +1,8 @@
+package interfaces
+
+import "github.com/maxwelbm/alkemy-g7.git/internal/model"
+
+type ICarriersRepo interface {
+	PostCarrier(newCarrier model.Carries) (id int64, err error)
+	GetById(id int) (carrier model.Carries, err error)
+}
