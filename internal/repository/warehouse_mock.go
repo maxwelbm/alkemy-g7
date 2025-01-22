eced97c4532c75b7b631d82dc4a3e14c36dfb472
Thiago Vieira <thiivieira@gmail.com>
2025-01-22 16:08:41 -0300

    test/feat: implement mockRepository getByIdWareHouse

--- internal/repository/warehouse_mock.go
+++ internal/repository/warehouse_mock.go
@@ -9,12 +9,10 @@ type WareHouseMockRepo struct {
 	mock.Mock
 }
 
-// DeleteByIdWareHouse implements interfaces.IWarehouseRepo.
 func (w *WareHouseMockRepo) DeleteByIdWareHouse(id int) error {
 	panic("unimplemented")
 }
 
-// GetAllWareHouse implements interfaces.IWarehouseRepo.
 func (mock *WareHouseMockRepo) GetAllWareHouse() (w []model.WareHouse, err error) {
 	args := mock.Called()
 
@@ -24,17 +22,19 @@ func (mock *WareHouseMockRepo) GetAllWareHouse() (w []model.WareHouse, err error
 	return
 }
 
-// GetByIdWareHouse implements interfaces.IWarehouseRepo.
-func (*WareHouseMockRepo) GetByIdWareHouse(id int) (w model.WareHouse, err error) {
-	panic("unimplemented")
+func (mock *WareHouseMockRepo) GetByIdWareHouse(id int) (w model.WareHouse, err error) {
+	args := mock.Called(id)
+
+	w = args.Get(0).(model.WareHouse)
+	err = args.Error(1)
+
+	return
 }
 
-// PostWareHouse implements interfaces.IWarehouseRepo.
 func (w *WareHouseMockRepo) PostWareHouse(warehouse *model.WareHouse) (id int64, err error) {
 	panic("unimplemented")
 }
 
-// UpdateWareHouse implements interfaces.IWarehouseRepo.
 func (w *WareHouseMockRepo) UpdateWareHouse(id int, warehouse *model.WareHouse) (err error) {
 	panic("unimplemented")
 }
