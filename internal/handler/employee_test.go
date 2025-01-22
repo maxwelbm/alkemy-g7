fa395161baf963370ed7432d67c8b3b6f0222702
miguel <mi_lmc@hotmail.com>
2025-01-22 10:24:04 -0300

    test: getEmployeeById handler unit tests

--- internal/handler/employee_test.go
+++ internal/handler/employee_test.go
@@ -5,7 +5,9 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/maxwelbm/alkemy-g7.git/internal/model"
+	"github.com/maxwelbm/alkemy-g7.git/pkg/custom_error"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -24,9 +26,52 @@ func TestGetEmployeesHandler(t *testing.T) {
 		res := httptest.NewRecorder()
 
 		employeeHd.GetEmployeesHandler(res, req)
-
+		expected := `{"data":[{"id":1,"card_number_id":"1","first_name":"John","last_name":"Cena","warehouse_id":1},{"id":2,"card_number_id":"2","first_name":"Martha","last_name":"Piana","warehouse_id":2}]}`
 		assert.Equal(t, res.Code, http.StatusOK)
-		assert.Equal(t, res.Body.String(), `{"data":[{"id":1,"card_number_id":"1","first_name":"John","last_name":"Cena","warehouse_id":1},{"id":2,"card_number_id":"2","first_name":"Martha","last_name":"Piana","warehouse_id":2}]}`)
+		assert.Equal(t, res.Body.String(), expected)
+	})
+}
+
+func TestGetEmployeeById(t *testing.T) {
+	getById := func(id int) (model.Employee, error) {
+		return model.Employee{Id: 1, CardNumberId: "1", FirstName: "John", LastName: "Cena", WarehouseId: 1}, nil
+	}
+	employeeHd := EmployeeHandler{
+		sv: &StubMockService{FuncGetEmployeeById: getById},
+	}
+
+	r := chi.NewRouter()
+	r.Get("/api/v1/employees/{id}", employeeHd.GetEmployeeById)
+
+	t.Run("should return the employee requested and 200 ok", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/api/v1/employees/1", nil)
+		res := httptest.NewRecorder()
+
+		r.ServeHTTP(res, req)
+
+		expected := `{"data":{"id":1,"card_number_id":"1","first_name":"John","last_name":"Cena","warehouse_id":1}}`
+		assert.Equal(t, http.StatusOK, res.Code)
+		assert.Equal(t, res.Body.String(), expected)
+	})
+
+	t.Run("should return a not found when employee id not exists", func(t *testing.T) {
+		getById := func(id int) (model.Employee, error) {
+			return model.Employee{}, custom_error.EmployeeErrNotFound
+		}
+		employeeHd := EmployeeHandler{
+			sv: &StubMockService{FuncGetEmployeeById: getById},
+		}
+
+		r.Get("/api/v1/employees/{id}", employeeHd.GetEmployeeById)
+
+		req := httptest.NewRequest("GET", "/api/v1/employees/2", nil)
+		res := httptest.NewRecorder()
+
+		r.ServeHTTP(res, req)
+
+		expected := `{"message":"employee not found"}`
+		assert.Equal(t, http.StatusNotFound, res.Code)
+		assert.Equal(t, expected, res.Body.String())
 	})
 }
 
