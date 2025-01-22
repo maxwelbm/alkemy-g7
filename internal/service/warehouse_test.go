e4bbe0798304e469660236492d3fc3372534d8e9
Thiago Vieira <thiivieira@gmail.com>
2025-01-22 16:09:31 -0300

    test/feat: implement warehouseTest in service TestGetByIdWareHouse

--- internal/service/warehouse_test.go
+++ internal/service/warehouse_test.go
@@ -61,3 +61,39 @@ func TestGetAllWarehouse(t *testing.T) {
 		mockRepo.AssertExpectations(t)
 	})
 }
+
+func TestGetByIdWareHouse(t *testing.T) {
+	t.Run("GetById", func(t *testing.T) {
+		svc := setup()
+
+		expectWarehouse := model.WareHouse{
+			Id:                 1,
+			WareHouseCode:      "test",
+			Telephone:          "test",
+			MinimunCapacity:    1,
+			MinimunTemperature: 1,
+			Address:            "test",
+		}
+		mockRepo := svc.Rp.(*repository.WareHouseMockRepo)
+		mockRepo.On("GetByIdWareHouse", 1).Return(expectWarehouse, nil)
+
+		w, err := svc.GetByIdWareHouse(1)
+
+		assert.Equal(t, expectWarehouse, w)
+		assert.Nil(t, err)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("GetByIdError", func(t *testing.T) {
+		svc := setup()
+
+		mockRepo := svc.Rp.(*repository.WareHouseMockRepo)
+		mockRepo.On("GetByIdWareHouse", 1).Return(model.WareHouse{}, assert.AnError)
+
+		w, err := svc.GetByIdWareHouse(1)
+
+		assert.Empty(t, w)
+		assert.NotNil(t, err)
+		mockRepo.AssertExpectations(t)
+	})
+}
