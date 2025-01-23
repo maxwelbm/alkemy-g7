ad3f884c8b17a3fe763762fb8ea1189f57a45d24
Thiago Vieira <thiivieira@gmail.com>
2025-01-23 19:27:19 -0300

    test/feat: create new warehousemock service for handler

---
+++ internal/service/warehouse_mock.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"github.com/maxwelbm/alkemy-g7.git/internal/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type WarehouseServiceMock struct {
+	mock.Mock
+}
+
+// DeleteByIdWareHouse implements interfaces.IWarehouseService.
+func (mock *WarehouseServiceMock) DeleteByIdWareHouse(id int) error {
+	panic("unimplemented")
+}
+
+// GetByIdWareHouse implements interfaces.IWarehouseService.
+func (mock *WarehouseServiceMock) GetByIdWareHouse(id int) (w model.WareHouse, err error) {
+	panic("unimplemented")
+}
+
+// PostWareHouse implements interfaces.IWarehouseService.
+func (mock *WarehouseServiceMock) PostWareHouse(warehouse model.WareHouse) (w model.WareHouse, err error) {
+	panic("unimplemented")
+}
+
+// UpdateWareHouse implements interfaces.IWarehouseService.
+func (mock *WarehouseServiceMock) UpdateWareHouse(id int, warehouse model.WareHouse) (w model.WareHouse, err error) {
+	panic("unimplemented")
+}
+
+func (mock *WarehouseServiceMock) GetAllWareHouse() (w []model.WareHouse, err error) {
+	args := mock.Called()
+
+	w = args.Get(0).([]model.WareHouse)
+	err = args.Error(1)
+
+	return
+}
