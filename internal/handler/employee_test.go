0d71b17f047e915ed12b94ff961d6599ba73abb8
miguel <mi_lmc@hotmail.com>
2025-01-24 16:31:18 -0300

    test: covering errors in updateEmployee handler test

--- internal/handler/employee_test.go
+++ internal/handler/employee_test.go
@@ -309,6 +309,51 @@ func TestUpdateEmployee(t *testing.T) {
 		assert.Equal(t, http.StatusNotFound, res.Code)
 		assert.Equal(t, res.Body.String(), expected)
 	})
+
+	t.Run("should return an error in case of invalid id type", func(t *testing.T) {
+		req := httptest.NewRequest("PATCH", "/api/v1/employees/something", nil)
+		res := httptest.NewRecorder()
+
+		r.Patch("/api/v1/employees/{id}", employeeHd.UpdateEmployee)
+
+		r.ServeHTTP(res, req)
+
+		expected := `{"message":"error parsing the id in path param"}`
+		assert.Equal(t, http.StatusBadRequest, res.Code)
+		assert.Equal(t, expected, res.Body.String())
+	})
+
+	t.Run("should return an error in case of invalid requestBody", func(t *testing.T) {
+		req := updateRequest("")
+		res := httptest.NewRecorder()
+
+		r.Patch("/api/v1/employees/{id}", employeeHd.UpdateEmployee)
+
+		r.ServeHTTP(res, req)
+
+		expected := `{"message":"error parsing the request body"}`
+		assert.Equal(t, http.StatusBadRequest, res.Code)
+		assert.Equal(t, expected, res.Body.String())
+	})
+
+	t.Run("should return 500 internal error in case of unexpected error", func(t *testing.T) {
+		updateEmployee := func(id int, employee model.Employee) (model.Employee, error) {
+			return model.Employee{}, errors.New("unexpected error")
+		}
+		employeeHd := EmployeeHandler{
+			sv: &StubMockService{FuncUpdateEmployee: updateEmployee},
+		}
+
+		req := updateRequest(newEmployee)
+		res := httptest.NewRecorder()
+
+		r.Patch("/api/v1/employees/{id}", employeeHd.UpdateEmployee)
+		r.ServeHTTP(res, req)
+
+		expected := `{"message":"something went wrong"}`
+		assert.Equal(t, res.Code, http.StatusInternalServerError)
+		assert.Equal(t, res.Body.String(), expected)
+	})
 }
 
 func TestDeleteEmployee(t *testing.T) {
