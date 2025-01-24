65a8e9143374100101fa2cbeb5ce27f2b819d043
Thiago Vieira <thiivieira@gmail.com>
2025-01-24 12:17:02 -0300

    chore: remove comments

--- internal/service/warehouse_mock.go
+++ internal/service/warehouse_mock.go
@@ -9,22 +9,18 @@ type WarehouseServiceMock struct {
 	mock.Mock
 }
 
-// DeleteByIdWareHouse implements interfaces.IWarehouseService.
 func (mock *WarehouseServiceMock) DeleteByIdWareHouse(id int) error {
 	panic("unimplemented")
 }
 
-// GetByIdWareHouse implements interfaces.IWarehouseService.
 func (mock *WarehouseServiceMock) GetByIdWareHouse(id int) (w model.WareHouse, err error) {
 	panic("unimplemented")
 }
 
-// PostWareHouse implements interfaces.IWarehouseService.
 func (mock *WarehouseServiceMock) PostWareHouse(warehouse model.WareHouse) (w model.WareHouse, err error) {
 	panic("unimplemented")
 }
 
-// UpdateWareHouse implements interfaces.IWarehouseService.
 func (mock *WarehouseServiceMock) UpdateWareHouse(id int, warehouse model.WareHouse) (w model.WareHouse, err error) {
 	panic("unimplemented")
 }
