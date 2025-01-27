f1ccc78fbb7b75914c30db9067b16cc249aedae2
Daniel Filho <daniel.agff@gmail.com>
2025-01-27 09:47:33 -0300

    Feat: Adding tag to swagger

--- internal/handler/product.go
+++ internal/handler/product.go
@@ -21,6 +21,14 @@ func NewProductHandler(ps interfaces.IProductService) *ProductHandler {
 	return &ProductHandler{ProductService: ps}
 }
 
+// GetAllProducts retrieves all products.
+// @Summary Retrieve all products
+// @Description Fetch all registered products from the database
+// @Tags Product
+// @Produce json
+// @Success 200 {object} model.ProductResponseSwagger
+// @Failure 500 {object} model.ErrorResponseSwagger "Unable to list products"
+// @Router /products [get]
 func (ph *ProductHandler) GetAllProducts(w http.ResponseWriter, r *http.Request) {
 	products, err := ph.ProductService.GetAllProducts()
 
@@ -32,6 +40,17 @@ func (ph *ProductHandler) GetAllProducts(w http.ResponseWriter, r *http.Request)
 	response.JSON(w, http.StatusOK, responses.CreateResponseBody("", products))
 }
 
+// GetProductByID retrieves a product by its ID.
+// @Summary Retrieve a product
+// @Description This endpoint fetches the details of a specific product based on the provided product ID.
+// @Tags Product
+// @Produce json
+// @Param id path int true "Product ID"
+// @Success 200 {object} model.ProductResponseSwagger{data=model.Product}
+// @Failure 400 {object} model.ErrorResponseSwagger "Invalid ID"
+// @Failure 404 {object} model.ErrorResponseSwagger "Product Not Found"
+// @Failure 500 {object} model.ErrorResponseSwagger "Unable to search for product"
+// @Router /products/{id} [get]
 func (ph *ProductHandler) GetProductByID(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
 
@@ -50,6 +69,17 @@ func (ph *ProductHandler) GetProductByID(w http.ResponseWriter, r *http.Request)
 	response.JSON(w, http.StatusOK, responses.CreateResponseBody("", product))
 }
 
+// DeleteProductByID deletes a product by its ID.
+// @Summary Delete a product by ID
+// @Description This endpoint allows for deleting a product based on the provided product ID.
+// @Tags Product
+// @Produce json
+// @Param id path int true "Product ID"
+// @Success 204 {object} nil "Product successfully deleted"
+// @Failure 400 {object} model.ErrorResponseSwagger "Invalid ID"
+// @Failure 404 {object} model.ErrorResponseSwagger "Product not found"
+// @Failure 500 {object} model.ErrorResponseSwagger "Unable to delete product"
+// @Router /products/{id} [delete]
 func (ph *ProductHandler) DeleteProductByID(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
 
@@ -74,6 +104,16 @@ func (ph *ProductHandler) DeleteProductByID(w http.ResponseWriter, r *http.Reque
 	response.JSON(w, http.StatusNoContent, responses.CreateResponseBody("product deleted", nil))
 }
 
+// CreateProduct creates a new product.
+// @Summary Create a new product
+// @Description This endpoint allows for creating a new product.
+// @Tags Product
+// @Produce json
+// @Param product body model.Product true "Product information"
+// @Success 201 {object} model.ProductResponseSwagger{data=model.Product}
+// @Failure 400 {object} model.ErrorResponseSwagger "Invalid input"
+// @Failure 500 {object} model.ErrorResponseSwagger "Unable to create product"
+// @Router /products [post]
 func (ph *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	var productBody model.Product
 
@@ -92,6 +132,18 @@ func (ph *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) 
 	response.JSON(w, http.StatusCreated, responses.CreateResponseBody("", product))
 }
 
+// UpdateProduct updates an existing product.
+// @Summary Update an existing product
+// @Description This endpoint allows for updating the details of a specific product identified by the provided ID.
+// @Tags Product
+// @Produce json
+// @Param id path int true "Product ID"
+// @Param product body model.Product true "Product information"
+// @Success 200 {object} model.ProductResponseSwagger{data=model.Product} "Product successfully updated"
+// @Failure 400 {object} model.ErrorResponseSwagger "Invalid ID"
+// @Failure 404 {object} model.ErrorResponseSwagger "Product not found"
+// @Failure 500 {object} model.ErrorResponseSwagger "Unable to update product"
+// @Router /products/{id} [patch]
 func (ph *ProductHandler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
 
--- internal/handler/productrec.go
+++ internal/handler/productrec.go
@@ -20,12 +20,21 @@ func NewProductRecHandler(prs interfaces.IProductRecService) *ProductRecHandler 
 	return &ProductRecHandler{ProductRecServ: prs}
 }
 
+// CreateProductRecServ creates a new product record.
+// @Summary Create a new product record
+// @Description This endpoint allows for creating a new product record.
+// @Tags ProductRecord
+// @Produce json
+// @Param productRecord body model.ProductRecords true "Product Record Information"
+// @Success 201 {object} model.ProductRecordResponseSwagger{data=model.ProductRecords}
+// @Failure 422 {object} model.ErrorResponseSwagger "Invalid JSON format"
+// @Failure 500 {object} model.ErrorResponseSwagger "Unable to create product record"
+// @Router /product-records [post]
 func (prh *ProductRecHandler) CreateProductRecServ(w http.ResponseWriter, r *http.Request) {
 	var productRecBody model.ProductRecords
 
 	if err := json.NewDecoder(r.Body).Decode(&productRecBody); err != nil {
 		response.JSON(w, http.StatusUnprocessableEntity, responses.CreateResponseBody("json mal formatado ou invalido", nil))
-
 		return
 	}
 
@@ -33,18 +42,26 @@ func (prh *ProductRecHandler) CreateProductRecServ(w http.ResponseWriter, r *htt
 	if err != nil {
 		if err, ok := err.(*customError.GenericError); ok {
 			response.JSON(w, err.Code, responses.CreateResponseBody(err.Error(), nil))
-
 			return
 		}
 
 		response.JSON(w, http.StatusInternalServerError, responses.CreateResponseBody(customError.ErrUnknow.Error(), nil))
-
 		return
 	}
 
 	response.JSON(w, http.StatusCreated, responses.CreateResponseBody("", product))
 }
 
+// GetProductRecReport retrieves a product record report.
+// @Summary Retrieve a product record report
+// @Description This endpoint retrieves the product record report based on the provided product ID. If no ID is provided, it returns all records.
+// @Tags ProductRecord
+// @Produce json
+// @Param id query int false "Product ID"
+// @Success 200 {object} model.ProductRecordResponseSwagger{data=[]model.ProductRecords}
+// @Failure 400 {object} model.ErrorResponseSwagger "Invalid Parameter"
+// @Failure 500 {object} model.ErrorResponseSwagger "Internal Server Error"
+// @Router /product-records/report [get]
 func (prh *ProductRecHandler) GetProductRecReport(w http.ResponseWriter, r *http.Request) {
 	idProductStr := r.URL.Query().Get("id")
 	idProduct := 0
@@ -68,7 +85,6 @@ func (prh *ProductRecHandler) GetProductRecReport(w http.ResponseWriter, r *http
 		}
 
 		response.JSON(w, http.StatusInternalServerError, responses.CreateResponseBody("Internal Server Error", nil))
-		
 		return
 	}
 
