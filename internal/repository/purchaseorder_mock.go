26f41e49b0a970ae3146a336db0785711073249e
Alef <alefsa_daniel@hotmail.com>
2025-01-27 02:27:19 -0300

    feat: create repository mock purchaseOrder

---
+++ internal/repository/purchaseorder_mock.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"github.com/maxwelbm/alkemy-g7.git/internal/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type PurchaseOrderRepositoryMock struct {
+	mock.Mock
+}
+
+func (p PurchaseOrderRepositoryMock) GetById(id int) (purchaseOrder model.PurchaseOrder, err error) {
+	args := p.Called(id)
+	purchaseOrder = args.Get(0).(model.PurchaseOrder)
+	err = args.Error(1)
+	return
+}
+
+func (p PurchaseOrderRepositoryMock) Post(newPurchaseOrder model.PurchaseOrder) (id int64, err error) {
+	args := p.Called(newPurchaseOrder)
+	id = args.Get(0).(int64)
+	err = args.Error(1)
+	return
+}
