8e1bff6df816c29d0c7e32492607e4d230a5539a
Alexandre Junior <atlopesjunior@gmail.com>
2025-02-17 18:42:21 -0300

    tests: removed old mocks, adjusted tests to use mockery mocks and add new cases to amplify coverage

--- internal/handler/section_test.go
+++ internal/handler/section_test.go
@@ -8,25 +8,25 @@ import (
 	"testing"
 
 	"github.com/maxwelbm/alkemy-g7.git/internal/handler"
+	"github.com/maxwelbm/alkemy-g7.git/internal/mocks"
 	"github.com/maxwelbm/alkemy-g7.git/internal/model"
-	"github.com/maxwelbm/alkemy-g7.git/internal/service"
 	"github.com/maxwelbm/alkemy-g7.git/pkg/customerror"
 	"github.com/stretchr/testify/assert"
 )
 
-func setupSectionService() *handler.SectionController {
-	mockSectionService := new(service.MockSectionService)
+func setupSectionService(t *testing.T) *handler.SectionController {
+	mockSectionService := mocks.NewMockISectionService(t)
 	hd := handler.CreateHandlerSections(mockSectionService)
 	return hd
 }
 
 func TestHandlerGet(t *testing.T) {
 	t.Run("return a list of all existing sections successfully", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
 		expectedSections := []model.Section{{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}, {ID: 2, SectionNumber: "S02", CurrentTemperature: 15.0, MinimumTemperature: 10.0, CurrentCapacity: 20, MinimumCapacity: 10, MaximumCapacity: 30, WarehouseID: 2, ProductTypeID: 2}}
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("Get").Return(expectedSections, nil)
 
 		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections", nil)
@@ -67,9 +67,9 @@ func TestHandlerGet(t *testing.T) {
 	})
 
 	t.Run("return an error when fetching sections", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("Get").Return([]model.Section{}, errors.New("unable to list sections"))
 
 		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections", nil)
@@ -86,11 +86,11 @@ func TestHandlerGet(t *testing.T) {
 
 func TestHandlerGetSectionByID(t *testing.T) {
 	t.Run("return section by id if it exist", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
 		expectedSection := model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("GetByID", expectedSection.ID).Return(expectedSection, nil)
 
 		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/1", nil)
@@ -117,9 +117,9 @@ func TestHandlerGetSectionByID(t *testing.T) {
 	})
 
 	t.Run("return section not found", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("GetByID", 100).Return(model.Section{}, customerror.HandleError("section", customerror.ErrorNotFound, ""))
 
 		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/100", nil)
@@ -135,7 +135,7 @@ func TestHandlerGetSectionByID(t *testing.T) {
 	})
 
 	t.Run("given an invalid id then return error", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
 		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/aaa", nil)
 		response := httptest.NewRecorder()
@@ -147,15 +147,33 @@ func TestHandlerGetSectionByID(t *testing.T) {
 		assert.Equal(t, http.StatusBadRequest, response.Code)
 		assert.JSONEq(t, expectedJSON, response.Body.String())
 	})
+
+	t.Run("return unable to search for section", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		mockService := hd.Sv.(*mocks.MockISectionService)
+		mockService.On("GetByID", 100).Return(model.Section{}, errors.New("unable to search for section"))
+
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/100", nil)
+		response := httptest.NewRecorder()
+
+		hd.GetByID(response, request)
+
+		expectedSectionJSON := `{"message": "unable to search for section"}`
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
+		mockService.AssertExpectations(t)
+	})
 }
 
 func TestHandlerCreateSection(t *testing.T) {
 	t.Run("given a valid section then create successfully", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
 		expectedSection := model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("Post", &model.Section{SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}).Return(expectedSection, nil)
 
 		reqBody := []byte(`{
@@ -193,7 +211,7 @@ func TestHandlerCreateSection(t *testing.T) {
 	})
 
 	t.Run("given an invalid section field then return a error", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 		reqBody := []byte(`{
 			"section_number": "",
 			"current_temperature": 0.0,
@@ -212,9 +230,9 @@ func TestHandlerCreateSection(t *testing.T) {
 	})
 
 	t.Run("given a valid section that already exist then return error", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("Post", &model.Section{SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}).Return(model.Section{}, customerror.HandleError("section", customerror.ErrorConflict, ""))
 
 		reqBody := []byte(`{
@@ -239,15 +257,66 @@ func TestHandlerCreateSection(t *testing.T) {
 		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
 		mockService.AssertExpectations(t)
 	})
+
+	t.Run("given an invalid json then return error", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		reqBody := []byte(`{
+			"section_number": "S01",
+			"current_temperature": 10.0,
+			InvalidField
+		}`)
+
+		request := httptest.NewRequest(http.MethodPost, "/api/v1/sections/", bytes.NewReader(reqBody))
+		response := httptest.NewRecorder()
+
+		hd.Post(response, request)
+
+		expectedSectionJSON := `{"message": "invalid request body"}`
+
+		assert.Equal(t, http.StatusUnprocessableEntity, response.Code)
+		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
+	})
+
+	t.Run("return unable to create section", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		expSection := model.Section{SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
+
+		reqBody := []byte(`{
+			"section_number": "S01",
+			"current_temperature": 10.0,
+			"minimum_temperature": 5.0,
+			"current_capacity": 10,
+			"minimum_capacity": 5,
+			"maximum_capacity": 20,
+			"warehouse_id": 1,
+			"product_type_id": 1
+		}`)
+
+		mockService := hd.Sv.(*mocks.MockISectionService)
+		mockService.On("Post", &expSection).Return(model.Section{}, errors.New("unable to create section"))
+
+		request := httptest.NewRequest(http.MethodPost, "/api/v1/sections/", bytes.NewReader(reqBody))
+		response := httptest.NewRecorder()
+
+		hd.Post(response, request)
+
+		expectedSectionJSON := `{"message": "unable to create section"}`
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
+		mockService.AssertExpectations(t)
+	})
 }
 
 func TestHandlerUpdateSection(t *testing.T) {
 	t.Run("given a valid section to update then update it with no error", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
 		updatedSection := model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 12.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("Update", 1, &model.Section{ID: 1, CurrentTemperature: 14.0}).Return(updatedSection, nil)
 
 		reqBody := []byte(`{"current_temperature": 14.0}`)
@@ -278,9 +347,9 @@ func TestHandlerUpdateSection(t *testing.T) {
 	})
 
 	t.Run("given a invalid section to update then return an error", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("Update", 50, &model.Section{ID: 50, CurrentTemperature: 5.0}).Return(model.Section{}, customerror.HandleError("section", customerror.ErrorNotFound, ""))
 
 		reqBody := []byte(`{
@@ -298,13 +367,84 @@ func TestHandlerUpdateSection(t *testing.T) {
 		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
 		mockService.AssertExpectations(t)
 	})
+
+	t.Run("given an invalid param then return an error", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		request := httptest.NewRequest(http.MethodPatch, "/api/v1/sections/aaa", nil)
+		response := httptest.NewRecorder()
+
+		hd.Update(response, request)
+
+		expectedSectionJSON := `{"message": "invalid id param"}`
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
+	})
+
+	t.Run("given an invalid request body then return an error", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		reqBody := []byte(`{
+			"current_temperature": 5.0,
+			InvalidRequest
+		}`)
+
+		request := httptest.NewRequest(http.MethodPatch, "/api/v1/sections/50", bytes.NewReader(reqBody))
+		response := httptest.NewRecorder()
+
+		hd.Update(response, request)
+
+		expectedSectionJSON := `{"message": "invalid request body"}`
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
+	})
+
+	t.Run("given an empty request body then return an error", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		reqBody := []byte(`{}`)
+
+		request := httptest.NewRequest(http.MethodPatch, "/api/v1/sections/50", bytes.NewReader(reqBody))
+		response := httptest.NewRecorder()
+
+		hd.Update(response, request)
+
+		expectedSectionJSON := `{"message": "request body cannot be empty"}`
+
+		assert.Equal(t, http.StatusUnprocessableEntity, response.Code)
+		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
+	})
+
+	t.Run("given an invalid section to update then return an error", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		mockService := hd.Sv.(*mocks.MockISectionService)
+		mockService.On("Update", 50, &model.Section{ID: 50, CurrentTemperature: 5.0}).Return(model.Section{}, errors.New("unable to update section"))
+
+		reqBody := []byte(`{
+			"current_temperature": 5.0
+		}`)
+
+		request := httptest.NewRequest(http.MethodPatch, "/api/v1/sections/50", bytes.NewReader(reqBody))
+		response := httptest.NewRecorder()
+
+		hd.Update(response, request)
+
+		expectedSectionJSON := `{"message": "unable to update section"}`
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
+		mockService.AssertExpectations(t)
+	})
 }
 
 func TestHandlerDeleteSection(t *testing.T) {
 	t.Run("given a valid section id then delete this section", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("Delete", 1).Return(nil)
 
 		request := httptest.NewRequest(http.MethodDelete, "/api/v1/sections/1", nil)
@@ -317,9 +457,9 @@ func TestHandlerDeleteSection(t *testing.T) {
 	})
 
 	t.Run("given an invalid section id then return error", func(t *testing.T) {
-		hd := setupSectionService()
+		hd := setupSectionService(t)
 
-		mockService := hd.Sv.(*service.MockSectionService)
+		mockService := hd.Sv.(*mocks.MockISectionService)
 		mockService.On("Delete", 50).Return(customerror.HandleError("section", customerror.ErrorNotFound, ""))
 
 		request := httptest.NewRequest(http.MethodDelete, "/api/v1/sections/50", nil)
@@ -332,5 +472,168 @@ func TestHandlerDeleteSection(t *testing.T) {
 		assert.Equal(t, http.StatusNotFound, response.Code)
 		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
 		mockService.AssertExpectations(t)
+	})
+
+	t.Run("given an invalid section id then return error", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		request := httptest.NewRequest(http.MethodDelete, "/api/v1/sections/aaa", nil)
+		response := httptest.NewRecorder()
+
+		hd.Delete(response, request)
+
+		expectedSectionJSON := `{"message": "invalid id param"}`
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
+	})
+
+	t.Run("given an invalid section then return error", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		mockService := hd.Sv.(*mocks.MockISectionService)
+		mockService.On("Delete", 50).Return(errors.New("unable to delete section"))
+
+		request := httptest.NewRequest(http.MethodDelete, "/api/v1/sections/50", nil)
+		response := httptest.NewRecorder()
+
+		hd.Delete(response, request)
+
+		expectedSectionJSON := `{"message": "unable to delete section"}`
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+		assert.JSONEq(t, expectedSectionJSON, response.Body.String())
+		mockService.AssertExpectations(t)
+	})
+}
+
+func TestHandlerCountProductBatchesSections(t *testing.T) {
+	t.Run("return the count of all product batches successfully", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		countProductBatchesSections := []model.SectionProductBatches{
+			{ID: 1, SectionNumber: "S01", ProductsCount: 1},
+		}
+
+		mockService := hd.Sv.(*mocks.MockISectionService)
+		mockService.On("CountProductBatchesSections").Return(countProductBatchesSections, nil)
+
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/reportProducts", nil)
+		response := httptest.NewRecorder()
+
+		hd.CountProductBatchesSections(response, request)
+
+		expectedJson := `{
+    "data": [
+        {
+            "id": 1,
+            "section_number": "S01",
+            "products_count": 1
+        }
+    ]
+}`
+
+		assert.Equal(t, http.StatusOK, response.Code)
+		assert.JSONEq(t, expectedJson, response.Body.String())
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("return a error when count product batches", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		mockService := hd.Sv.(*mocks.MockISectionService)
+		mockService.On("CountProductBatchesSections").Return([]model.SectionProductBatches{}, errors.New("unable to count section product batches"))
+
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/reportProducts", nil)
+		response := httptest.NewRecorder()
+
+		hd.CountProductBatchesSections(response, request)
+
+		expectedJson := `{
+    "message": "unable to count section product batches"
+}`
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+		assert.JSONEq(t, expectedJson, response.Body.String())
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("return the count of a product batches section successfully", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		countProductBatchesSection := model.SectionProductBatches{
+			ID: 1, SectionNumber: "S01", ProductsCount: 1,
+		}
+
+		mockService := hd.Sv.(*mocks.MockISectionService)
+		mockService.On("CountProductBatchesBySectionID", countProductBatchesSection.ID).Return(countProductBatchesSection, nil)
+
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/reportProducts?id=1", nil)
+		response := httptest.NewRecorder()
+
+		hd.CountProductBatchesSections(response, request)
+
+		expectedJson := `{
+			"data":
+			  {
+				"id": 1,
+				"products_count": 1,
+				"section_number": "S01"
+			  }
+		  }`
+
+		assert.Equal(t, http.StatusOK, response.Code)
+		assert.JSONEq(t, expectedJson, response.Body.String())
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("return the unmmaped error", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		mockService := hd.Sv.(*mocks.MockISectionService)
+		mockService.On("CountProductBatchesBySectionID", 1).Return(model.SectionProductBatches{}, errors.New("unable to count section product batches"))
+
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/reportProducts?id=1", nil)
+		response := httptest.NewRecorder()
+
+		hd.CountProductBatchesSections(response, request)
+
+		expectedJson := `{"message":"error"}`
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+		assert.JSONEq(t, expectedJson, response.Body.String())
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("return the custom error", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		mockService := hd.Sv.(*mocks.MockISectionService)
+		mockService.On("CountProductBatchesBySectionID", 1).Return(model.SectionProductBatches{}, customerror.HandleError("section", 0, ""))
+
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/reportProducts?id=1", nil)
+		response := httptest.NewRecorder()
+
+		hd.CountProductBatchesSections(response, request)
+
+		expectedJson := `{"message":"section unknow server error"}`
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+		assert.JSONEq(t, expectedJson, response.Body.String())
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("return error if id is invalid", func(t *testing.T) {
+		hd := setupSectionService(t)
+
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/sections/reportProducts?id=aaa", nil)
+		response := httptest.NewRecorder()
+
+		hd.CountProductBatchesSections(response, request)
+
+		expectedJson := `{"message":"invalid id"}`
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+		assert.JSONEq(t, expectedJson, response.Body.String())
 	})
 }
--- internal/service/sections_test.go
+++ internal/service/sections_test.go
@@ -3,25 +3,25 @@ package service_test
 import (
 	"testing"
 
+	"github.com/maxwelbm/alkemy-g7.git/internal/mocks"
 	"github.com/maxwelbm/alkemy-g7.git/internal/model"
-	"github.com/maxwelbm/alkemy-g7.git/internal/repository"
 	"github.com/maxwelbm/alkemy-g7.git/internal/service"
 	"github.com/maxwelbm/alkemy-g7.git/pkg/customerror"
 	"github.com/stretchr/testify/assert"
 )
 
-func setupRepMock() *service.SectionService {
-	mockRep := new(repository.MockSectionRepository)
+func setupRepMock(t *testing.T) *service.SectionService {
+	mockRep := mocks.NewMockISectionRepo(t)
 	return service.CreateServiceSection(mockRep)
 }
 
 func TestGetSections(t *testing.T) {
 	t.Run("return a list of all sections successfully", func(t *testing.T) {
-		svc := setupRepMock()
+		svc := setupRepMock(t)
 
 		expectedSections := []model.Section{{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}, {ID: 2, SectionNumber: "S02", CurrentTemperature: 15.0, MinimumTemperature: 10.0, CurrentCapacity: 20, MinimumCapacity: 10, MaximumCapacity: 30, WarehouseID: 2, ProductTypeID: 2}}
 
-		mockRepo := svc.Rp.(*repository.MockSectionRepository)
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
 		mockRepo.On("Get").Return(expectedSections, nil)
 
 		sections, err := svc.Get()
@@ -34,11 +34,11 @@ func TestGetSections(t *testing.T) {
 
 func TestSectionByID(t *testing.T) {
 	t.Run("given a valid section by id then return a section with no error", func(t *testing.T) {
-		svc := setupRepMock()
+		svc := setupRepMock(t)
 
 		expectedSection := model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
 
-		mockRepo := svc.Rp.(*repository.MockSectionRepository)
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
 		mockRepo.On("GetByID", 1).Return(expectedSection, nil)
 
 		section, err := svc.GetByID(1)
@@ -49,11 +49,11 @@ func TestSectionByID(t *testing.T) {
 	})
 
 	t.Run("given an invalid id return an error", func(t *testing.T) {
-		svc := setupRepMock()
+		svc := setupRepMock(t)
 
 		expectedError := customerror.HandleError("section", customerror.ErrorNotFound, "")
 
-		mockRepo := svc.Rp.(*repository.MockSectionRepository)
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
 		mockRepo.On("GetByID", 50).Return(model.Section{}, expectedError)
 
 		section, err := svc.GetByID(50)
@@ -66,11 +66,11 @@ func TestSectionByID(t *testing.T) {
 
 func TestPostSection(t *testing.T) {
 	t.Run("given a valid section create it successfully", func(t *testing.T) {
-		svc := setupRepMock()
+		svc := setupRepMock(t)
 
 		createdSection := model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
 
-		mockRepo := svc.Rp.(*repository.MockSectionRepository)
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
 		mockRepo.On("Post", &model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}).Return(createdSection, nil)
 
 		section, err := svc.Post(&createdSection)
@@ -81,12 +81,12 @@ func TestPostSection(t *testing.T) {
 	})
 
 	t.Run("given an invalid section return error", func(t *testing.T) {
-		svc := setupRepMock()
+		svc := setupRepMock(t)
 
 		expectedErrSection := customerror.HandleError("section", customerror.ErrorConflict, "")
 		createdSection := model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
 
-		mockRepo := svc.Rp.(*repository.MockSectionRepository)
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
 		mockRepo.On("Post", &createdSection).Return(model.Section{}, expectedErrSection)
 
 		section, err := svc.Post(&createdSection)
@@ -96,15 +96,26 @@ func TestPostSection(t *testing.T) {
 		assert.Error(t, err)
 		mockRepo.AssertExpectations(t)
 	})
+
+	t.Run("given an invalid section then return an empty section", func(t *testing.T) {
+		svc := setupRepMock(t)
+
+		createdSection := model.Section{ID: 1, CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
+
+		section, err := svc.Post(&createdSection)
+
+		assert.Equal(t, model.Section{}, section)
+		assert.Error(t, err)
+	})
 }
 
 func TestUpdateSection(t *testing.T) {
 	t.Run("given a valid section then update it", func(t *testing.T) {
-		svc := setupRepMock()
+		svc := setupRepMock(t)
 
 		updatedSection := model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
 
-		mockRepo := svc.Rp.(*repository.MockSectionRepository)
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
 		mockRepo.On("GetByID", 1).Return(updatedSection, nil)
 
 		mockRepo.On("Update", 1, &updatedSection).Return(updatedSection, nil)
@@ -117,10 +128,10 @@ func TestUpdateSection(t *testing.T) {
 	})
 
 	t.Run("given an invalid section id then return error", func(t *testing.T) {
-		svc := setupRepMock()
+		svc := setupRepMock(t)
 
 		updatedSection := model.Section{ID: 50, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
-		mockRepo := svc.Rp.(*repository.MockSectionRepository)
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
 
 		expectedError := customerror.HandleError("section", customerror.ErrorConflict, "")
 		mockRepo.On("GetByID", 50).Return(model.Section{}, expectedError)
@@ -136,10 +147,10 @@ func TestUpdateSection(t *testing.T) {
 
 func TestDeleteSection(t *testing.T) {
 	t.Run("given a valid id section then delete it", func(t *testing.T) {
-		svc := setupRepMock()
+		svc := setupRepMock(t)
 
 		deletedSection := model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
-		mockRepo := svc.Rp.(*repository.MockSectionRepository)
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
 
 		mockRepo.On("GetByID", 1).Return(deletedSection, nil)
 
@@ -154,11 +165,11 @@ func TestDeleteSection(t *testing.T) {
 	})
 
 	t.Run("given an invalid section id then return error", func(t *testing.T) {
-		svc := setupRepMock()
+		svc := setupRepMock(t)
 
 		expectedError := customerror.HandleError("section", customerror.ErrorNotFound, "")
 
-		mockRepo := svc.Rp.(*repository.MockSectionRepository)
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
 
 		mockRepo.On("GetByID", 50).Return(model.Section{}, expectedError)
 
@@ -166,6 +177,61 @@ func TestDeleteSection(t *testing.T) {
 
 		assert.ErrorIs(t, err, expectedError)
 		assert.Error(t, err)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("given a valid section id that have a dependecy with product batches then return error to delete", func(t *testing.T) {
+		svc := setupRepMock(t)
+
+		deletedSection := model.Section{ID: 1, SectionNumber: "S01", CurrentTemperature: 10.0, MinimumTemperature: 5.0, CurrentCapacity: 10, MinimumCapacity: 5, MaximumCapacity: 20, WarehouseID: 1, ProductTypeID: 1}
+		prodBatches := model.SectionProductBatches{ID: 1, SectionNumber: "S01", ProductsCount: 1}
+		expectedError := customerror.HandleError("section", customerror.ErrorDep, "")
+
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
+
+		mockRepo.On("GetByID", 1).Return(deletedSection, nil)
+
+		mockRepo.On("CountProductBatchesBySectionID", 1).Return(prodBatches, expectedError)
+
+		err := svc.Delete(1)
+
+		assert.Error(t, err)
+		mockRepo.AssertExpectations(t)
+	})
+}
+
+func TestCountProductBatchesBySectionID(t *testing.T) {
+	t.Run("given a valid product btaches section then return it", func(t *testing.T) {
+		svc := setupRepMock(t)
+
+		expectedSPB := model.SectionProductBatches{ID: 1, SectionNumber: "S01", ProductsCount: 1}
+
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
+
+		mockRepo.On("CountProductBatchesBySectionID", 1).Return(expectedSPB, nil)
+
+		count, err := svc.CountProductBatchesBySectionID(1)
+
+		assert.Equal(t, expectedSPB, count)
+		assert.NoError(t, err)
+		mockRepo.AssertExpectations(t)
+	})
+}
+
+func TestCountProductBatchesBySection(t *testing.T) {
+	t.Run("return a slice with all product batches sections", func(t *testing.T) {
+		svc := setupRepMock(t)
+
+		expectedSPB := []model.SectionProductBatches{{ID: 1, SectionNumber: "S01", ProductsCount: 1}, {ID: 2, SectionNumber: "S02", ProductsCount: 1}}
+
+		mockRepo := svc.Rp.(*mocks.MockISectionRepo)
+
+		mockRepo.On("CountProductBatchesSections").Return(expectedSPB, nil)
+
+		count, err := svc.CountProductBatchesSections()
+
+		assert.Equal(t, expectedSPB, count)
+		assert.NoError(t, err)
 		mockRepo.AssertExpectations(t)
 	})
 }
